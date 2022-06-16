1232b60a347d51e7fde307a1250e2cc69083f847
iankloo <iankloo@fastmail.com>
2022-06-16 11:28:54 -0400

    bug fixes

--- elorater/settings.py
+++ elorater/settings.py
@@ -84,31 +84,28 @@ WSGI_APPLICATION = 'elorater.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 #local
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
-#db user: elo-app-admin
-#db pass: arpadchess111$$$
-
 DATABASES = {
     'default': {
-        'ENGINE': 'mssql',
-        'NAME': 'elo-app-db',
-        'USER': 'elo-app-admin',
-        'PASSWORD': 'arpadchess111$$$',
-        'HOST': 'tcp:elo-app-db.database.windows.net',
-        'PORT': '1433',
-        'OPTIONS': {
-            'driver': 'ODBC Driver 17 for SQL Server',
-            'MARS_Connection': 'True',
-        },
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'mssql',
+#         'NAME': 'elo-app-db',
+#         'USER': 'elo-app-admin',
+#         'PASSWORD': 'arpadchess111$$$',
+#         'HOST': 'tcp:elo-app-db.database.windows.net',
+#         'PORT': '1433',
+#         'OPTIONS': {
+#             'driver': 'ODBC Driver 17 for SQL Server',
+#             'MARS_Connection': 'True',
+#         },
+#     },
+# }
 
 
 
@@ -150,22 +147,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 #local
-# STATIC_URL = '/static/'
-# PROJECT_ROOT = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\','/')
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\','/')
+STATIC_URL = '/static/'
 
+#server
+# DEFAULT_FILE_STORAGE = 'elorater.custom_azure.AzureMediaStorage'
+# STATICFILES_STORAGE = 'elorater.custom_azure.AzureStaticStorage'
 
-DEFAULT_FILE_STORAGE = 'elorater.custom_azure.AzureMediaStorage'
-STATICFILES_STORAGE = 'elorater.custom_azure.AzureStaticStorage'
+# STATIC_LOCATION = "static"
+# MEDIA_LOCATION = "media"
 
-STATIC_LOCATION = "static"
-MEDIA_LOCATION = "media"
-
-AZURE_ACCOUNT_NAME = "elotesting"
-AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
-STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
+# AZURE_ACCOUNT_NAME = "elotesting"
+# AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
+# STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+# MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
 
 
 
--- frontend/static/frontend/js/functions/update_pair.js
+++ frontend/static/frontend/js/functions/update_pair.js
@@ -7,95 +7,113 @@ function update_pair(access_code){
 
     success: function (data){
 
-      $('.main').html(`
-        <div class = 'content'>
-          <h4 id = 'num_left'></h4>
-          <br>
-          <h2 id = 'question'></h2>
-          <br><br>
-          <div class="row">
-            <div class="col-sm-6">
-              <div class="card" id = 'name1_card'>
-                <div class="card-body">
-                  <center><h4 class="card-title stretched-link" id = 'name1'></h4></center>
+      if(data.length == 0){
+        $('#overlay').fadeOut()
+          $('.main').html(`
+            <div class = 'content'>
+              <h2>Your ratings have been captured - thank you!</h2>
+            </div>
+          `)
+      } else{
+        $('.main').html(`
+          <div class = 'content'>
+            <h4 id = 'num_left'></h4>
+            <br>
+            <h2 id = 'question'></h2>
+            <br><br>
+            <div class="row">
+              <div class="col-sm-6">
+                <div class="card" id = 'name1_card'>
+                  <div class="card-body">
+                    <center><h4 class="card-title stretched-link" id = 'name1'></h4></center>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="col-sm-6">
-              <div class="card" id = 'name2_card'>
-                <div class="card-body">
-                  <center><h4 class="card-title" id = 'name2'></h4></center>
+              <div class="col-sm-6">
+                <div class="card" id = 'name2_card'>
+                  <div class="card-body">
+                    <center><h4 class="card-title" id = 'name2'></h4></center>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div id="overlay" style="display:none;">
-          <div class="spinner"></div>
-        </div>
-      `)
+          <div id="overlay" style="display:none;">
+            <div class="spinner"></div>
+          </div>
+        `)
 
-      var index = Math.floor(Math.random() * data.length)
-      get_question(data[index].experiment_name)
+        var index = Math.floor(Math.random() * data.length)
 
-      //get timestamp when viewer first sees pair
-      start = Date.now()
-      if(Math.round(Math.random()) == 0){
-        $('#name1').text(data[index].name_1.first + ' ' + data[index].name_1.last)
-        $('#name2').text(data[index].name_2.first + ' ' + data[index].name_2.last)
+        $.ajax({
+          url: 'api/v1/exp?exp_id=' + data[index].experiment_name,
+          type: "GET",
+          dataType: "json",
 
-        //handle clicks on names
-        $('#name1_card').click(function(){
-          $('#overlay').fadeIn()
-          end = Date.now()
-          add_rating(data[index], data[index].name_1, start, end, access_code)
-        });
+          success: function (q_data){
+            $('#question').text(q_data[0].question)
 
-        $('#name2_card').click(function(){
-          $('#overlay').fadeIn()
-          end = Date.now()
-          add_rating(data[index], data[index].name_2, start, end, access_code)
-        });
-      } else{
-        $('#name2').text(data[index].name_1.first + ' ' + data[index].name_1.last)
-        $('#name1').text(data[index].name_2.first + ' ' + data[index].name_2.last)
+            //get timestamp when viewer first sees pair
+            start = Date.now()
+            if(Math.round(Math.random()) == 0){
+              $('#name1').text(data[index].name_1.first + ' ' + data[index].name_1.last)
+              $('#name2').text(data[index].name_2.first + ' ' + data[index].name_2.last)
 
-        //handle clicks on names
-        $('#name1_card').click(function(){
-          $('#overlay').fadeIn()
-          end = Date.now()
-          add_rating(data[index], data[index].name_2, start, end, access_code)
-        });
+              //handle clicks on names
+              $('#name1_card').click(function(){
+                $('#overlay').fadeIn()
+                end = Date.now()
+                add_rating(data[index], data[index].name_1, start, end, access_code)
+              });
 
-        $('#name2_card').click(function(){
-          $('#overlay').fadeIn()
-          end = Date.now()
-          add_rating(data[index], data[index].name_1, start, end, access_code)
-        });
+              $('#name2_card').click(function(){
+                $('#overlay').fadeIn()
+                end = Date.now()
+                add_rating(data[index], data[index].name_2, start, end, access_code)
+              });
+            } else{
+              $('#name2').text(data[index].name_1.first + ' ' + data[index].name_1.last)
+              $('#name1').text(data[index].name_2.first + ' ' + data[index].name_2.last)
+
+              //handle clicks on names
+              $('#name1_card').click(function(){
+                $('#overlay').fadeIn()
+                end = Date.now()
+                add_rating(data[index], data[index].name_2, start, end, access_code)
+              });
+
+              $('#name2_card').click(function(){
+                $('#overlay').fadeIn()
+                end = Date.now()
+                add_rating(data[index], data[index].name_1, start, end, access_code)
+              });
+            }
+
+            //set matches remaining counter
+            $('#num_left').text('Matches Remaining: ' + data.length)
+
+            //handle arrow keys - just automate clicking process
+            $(document).keydown(function(e) {
+              switch(e.which) {
+                case 37: // left
+                  $('#name1_card').click()
+                break;
+
+                case 38: // up
+                break;
+
+                case 39: // right
+                  $('#name2_card').click()
+                break;
+
+                default: return; // exit this handler for other keys
+              }
+
+              e.preventDefault(); // prevent the default action (scroll / move caret)
+            });
+          }
+        })
       }
-
-      //set matches remaining counter
-      $('#num_left').text('Matches Remaining: ' + data.length)
-
-      //handle arrow keys - just automate clicking process
-      $(document).keydown(function(e) {
-        switch(e.which) {
-          case 37: // left
-            $('#name1_card').click()
-          break;
-
-          case 38: // up
-          break;
-
-          case 39: // right
-            $('#name2_card').click()
-          break;
-
-          default: return; // exit this handler for other keys
-        }
-
-        e.preventDefault(); // prevent the default action (scroll / move caret)
-      });
     }
   })
 }
